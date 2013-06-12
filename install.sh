26524a9ad86a8b261b5910b195573c58c192a2cd
Leo Bergnéhr <leo@bergnehr.se>
2013-06-11 21:04:45 -0400

    Fix raw installer script

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 IFS=
 
+install_file () {
+  local file=$1
+
+  # Copy any existing file
+  mv $file ${file}.orig
+
+  curl ${urlRoot}${file} > ~/$file
+}
+
 echo Installing Leo\'s dotfiles for `whoami`...
 
-urlRoot = https://raw.github.com/lbergnehr/dotfiles/master/
+urlRoot=https://raw.github.com/lbergnehr/dotfiles/master/
 
+for f in .gitconfig .hgrc .vimrc .zshrc; do
+  # There might an empty string from that list...
+  if [ "$f" = "" ]; then
+    continue
+  fi
 
-
-for f in .gitconfig .hgrc .vimrc .zshrc do
-  if [ install_file $f ]; then
+  if ( install_file "$f" ); then
     echo Installed $file successfully
   else
     echo Could not install $file
@@ -16,12 +28,3 @@ for f in .gitconfig .hgrc .vimrc .zshrc do
 done
 
 echo Installation complete
-
-function install_file () {
-  file = $1
-
-  # Copy any existing file
-  mv $file ${file}.orig
-
-  curl ${urlRoot}${file} > ~/$file
-}
