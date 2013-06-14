7770bef3149624aa59b9ac674b7e6c49642324d8
Leo Bergnéhr <leo@Silmarilli.local>
2013-06-13 22:26:16 -0400

    Correct install script
    * Check out the repo in the current directory and go from there.

--- git_install.sh
+++ git_install.sh
@@ -8,22 +8,25 @@ install_file () {
   local file=${2:?}
 
   # Copy any existing file
+  oldFile=/Users/${user}/$file
   local backedUpFile=${file}.`date "+%H_%M_%S"`.orig
-  if ( mv "/Users/${user}/$file" "/Users/${user}/$backedUpFile" ); then
-    echo Backed up old $file to $backedUpFile
+  if ( mv "${oldFile}" "/Users/${user}/$backedUpFile" ); then
+    echo Backed up old \"${oldFile}\" to \"${backedUpFile}\"
   fi
 
   # Link the file from the repo to user dir
-  echo Linking ${dir}/$file to /Users/${user}/$file
-  ln -s "${dir}/$file" /Users/${user}/$file
+  sourceFile="${dir}/${file}"
+  newFile=$oldFile
+  echo Linking ${dir}/$file to $newFile
+  ln -s "${sourceFile}" $newFile
 }
 
-echo Installing Leo\'s dotfiles through git and symbolic links for $user...
+echo Installing Leo\'s dotfiles through git and symbolic links for ${user}...
 
 # Clone the repository to the user directory
 echo Cloning dotfiles repository...
-targetDir=${1:=/Users/leo/dotfiles}
-if !( git clone git@github.com:lbergnehr/dotfiles.git $targetDir ); then
+targetDir=dotfiles
+if !( git clone git@github.com:lbergnehr/${targetDir}.git ); then
   echo Could not clone repository
   echo Aborting installation
   exit 1
@@ -36,7 +39,7 @@ for f in .gitconfig .hgrc .vimrc .zshrc; do
     continue
   fi
 
-  if ( install_file "$targetDir" "$f" ); then
+  if ( install_file "`pwd`/$targetDir" "$f" ); then
     echo Installed $file successfully
   else
     echo Could not install $file
