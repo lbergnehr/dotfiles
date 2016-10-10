d965d3d810631fcb05dae2dc62463dbc824f6df1
Leo Bergnéhr <leo@bergnehr.se>
2016-10-10 21:40:12 +0200

    Fix vagrant script to not use vundle
    And fix a typo.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,16 +3,13 @@
 # Install oh my zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-# Install Vundle (vim plugin manager)
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # Install Selecta
 git clone https://github.com/garybernhardt/selecta.git ~/bin/selecta_repo && ln -s ~/bin/selecta_repo/selecta ~/bin/selecta
 
 # Clone the dotfiles and symlink them
-git clone https://github.com/lbergnehr/dotfiles.git ~/dotfiles
+git clone https://github.com/lbergnehr/dotfiles.git --recursive ~/dotfiles
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.hgrc ~/.hgrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 rm ~/.zshrc && ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.tmux.config ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
